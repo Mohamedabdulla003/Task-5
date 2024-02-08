37259bd6730b9a1236fbb4ef864cadfd4e24c670
Mohamedabdulla003 <abdullamohamed406@gmail.com>
2024-02-08 15:27:22 +0530

    first commit

---
+++ Resume JSON .js
@@ -0,0 +1,69 @@
+let resume = {
+"basics": {
+    "name": "Mohamed abdulla.S",
+    "label": "",
+    "picture": "",
+    "email": "abdullamohamed406@gmail.com",
+    "phone": "(+91)8543287033",
+    "degree": "",
+    "website": "Your website URL",
+    "summary": "learning with full stack devlopment coures in  Guvi ",
+    "location": {
+      "address": "kuthunaar street,Thirunalveli",
+      "postalCode": "627058",
+      "city": "chennai",
+      "countryCode": "India",
+      "region": "asia"
+    },
+    
+  },
+  "work": [
+    {
+      "company": "TVS comapany",
+      "position": "qulity audit",
+      "website": "www.Tvsemployer.com",
+      "startDate": " 15.5.2021",
+      "endDate": "14.5.2023",
+      "summary": " I am  Quality audit in armature line",
+      "highlights":["working with consenration,i am best quality employer in line"
+      ]
+    }
+  ],
+
+  "education": [
+    {
+      "institution": "arasu higherser scondary school",
+      "area": "chennai",
+      "studyType": "dayscolour",
+      "startDate": "14.4.2016",
+      "endDate": "28.3.2018",
+      "gpa": "",
+      "courses": [
+        "computer science"
+      ]
+    }
+  ],
+  "skills": [
+    {
+      "name": "Programming Languages",
+      "level": "",
+      "keywords": [
+        " Java', 'C++', etc"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "language": "Tamil,English",
+      "fluency": "Tamil"
+    }
+  ],
+  "interests": [
+    {
+      "name": "Sports,watching tv,",
+      "keywords": [
+        " 'Cricket', 'Football', 'Golf'"
+      ]
+    }
+  ]
+}
